ba8cddaea64d1b5396ab8d8725658845017e277f
Bamboomatic <bambynek.pawel@gmail.com>
2020-02-12 21:04:03 +0100

    first commit. Will rewrite GHBrowser to SVELTE

---
+++ src/components/Form.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let current;
+</script>
+
+<form class="field has-addons">
+  <div class="control">
+    <input
+      class="input username"
+      type="text"
+      placeholder="enter github username"
+      required />
+  </div>
+  <div class="control">
+    <button
+      type="submit"
+      class="button is-info load-username has-tooltip-right
+      has-tooltip-multiline"
+      data-tooltip="username can only contain characters: `a-z`, `0-9`, `-`, `_`">
+      Load
+    </button>
+  </div>
+</form>
